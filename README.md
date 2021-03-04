18021a14bc599a10950c20c49461489742ff7871
Alex Winkler <fo.nttax@yahoo.de>
2021-03-04 17:30:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# SqlLint
+![Code Style](https://github.com/Liquipedia/SqlLint/workflows/Code%20Style/badge.svg)
+
+This is a thin wrapper around the SqlParser from the [phpMyAdmin project][1] which can be used to lint any amount of sql files from the command line.
+
+## Installation
+
+Please use [Composer][2] to install:
+
+```sh
+composer require liquipedia/sqllint
+```
+
+## Usage
+
+### Command line utility
+
+```sh
+./vendor/bin/sqllint
+```
+
+### Via composer script
+
+Add this to your composer.json
+```
+{
+	"require-dev": {
+		"liquipedia/sqllint": "*"
+	},
+	"scripts": {
+		"sqllint": [
+			"sqllint"
+		]
+	}
+}
+```
+
+and run
+
+```sh
+composer sqllint
+```
+[1]:https://github.com/phpmyadmin/sql-parser
+[2]:https://getcomposer.org/
---
+++ src/SqlLint.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Liquipedia\SqlLint;
+
+use PhpMyAdmin\SqlParser\Lexer;
+use PhpMyAdmin\SqlParser\Parser;
+use PhpMyAdmin\SqlParser\Utils\Error as ParserError;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RecursiveRegexIterator;
+use RegexIterator;
+
+class SqlLint {
+
+	public static function lint() {
+		$return = 0;
+		$output = PHP_EOL;
+		$directory = new RecursiveDirectoryIterator( './' );
+		$iterator = new RecursiveIteratorIterator( $directory );
+		$regex = new RegexIterator( $iterator, '/^.+\.sql$/i', RecursiveRegexIterator::GET_MATCH );
+		$items = [];
+		foreach ( $regex as $item ) {
+			$items[] = $item[ 0 ];
+		}
+		foreach ( $items as $file ) {
+			$fileContent = file_get_contents( $file );
+			$lexer = new Lexer( $fileContent );
+			$parser = new Parser( $lexer->list );
+			$errors = ParserError::get( [ $lexer, $parser ] );
+			if ( count( $errors ) > 0 ) {
+				$countErrors = count( $errors );
+				$return = 1;
+				$output .= 'FILE: ' . realpath( $file ) . PHP_EOL;
+				$output .= str_repeat( '-', 70 ) . PHP_EOL;
+				$output .= 'FOUND ' . $countErrors . ' ERROR' . ( $countErrors === 1 ? '' : 'S' ) . PHP_EOL;
+				$output .= str_repeat( '-', 70 ) . PHP_EOL;
+				$output .= implode( PHP_EOL, ParserError::format( $errors ) );
+				$output .= PHP_EOL . PHP_EOL;
+				echo 'E';
+			} else {
+				echo '.';
+			}
+		}
+		echo $output;
+
+		return $return;
+	}
+
+}
